de03461be9cc1626c179c703f39c40e6e0145212
kaltwelten <kaltwelten@gmail.com>
2025-10-27 15:13:20 +0100

    Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# buffoo.nvim
+
+Daft Neovim plugin full of bugs and missing essential features. If it works, it merely provides means to arrange buffers.
+
+It's a utility library. Created with [incline.nvim](https://github.com/b0o/incline.nvim) in mind.
---
+++ lua/buffoo/init.lua
@@ -0,0 +1,211 @@
+local _bin_by_result = function(l, f)
+  local bins = {}
+  for i, x in ipairs(l) do
+    local r = f and f(x) or x
+    if bins[r] then
+      bins[r][#bins[r] + 1] = i
+    else
+      bins[r] = { i }
+    end
+  end
+  return bins
+end
+
+local _make_names_unique = function(buffers)
+  for _, b in ipairs(buffers) do b.name.unique = b.name.tail end
+  local tails = _bin_by_result(buffers, function(b) return b.name.tail end)
+  for _, same_tail in vim.iter(pairs(tails)):filter(function(_, l) return #l > 1 end) do
+    for _, b in ipairs(same_tail) do
+      buffers[b].name.unique = nil
+      if not buffers[b].name.segments then
+        buffers[b].name.segments = vim.iter(buffers[b].name.full:gmatch('~?/?[^/]*/')):totable()
+      end
+    end
+    local sweep = {}
+    local s = 1
+    while vim.tbl_contains(buffers, function(b) return not b.name.unique end, { predicate = true }) do
+      for i, b in ipairs(same_tail) do
+        if s >= #buffers[b].name.segments and not buffers[b].name.unique then
+          buffers[b].name.unique = buffers[b].name.full
+        end
+        sweep[i] = buffers[b].name.segments[s] or buffers[b].name.unique
+      end
+      local bins = _bin_by_result(sweep)
+      vim.iter(pairs(bins)):filter(function(_, l) return #l == 1 and not buffers[same_tail[l[1]]].name.unique end):each(function(_, l)
+        local b = same_tail[l[1]]
+        local segments = buffers[b].name.segments
+        buffers[b].name.unique = vim.iter(segments):rskip(#segments - s):join('') .. '…/' .. buffers[b].name.tail
+      end)
+      s = s + 1
+    end
+  end
+end
+
+local _idx = function(buf, buffers)
+  for i, b in ipairs(buffers) do
+    if b.buf == buf then return i end
+  end
+end
+
+local _bufs_listed = function()
+  return vim.tbl_filter(function(buf) return vim.fn.buflisted(buf) == 1 end, vim.api.nvim_list_bufs())
+end
+
+local _populate = vim.schedule_wrap(function(buffers)
+  for i, b in ipairs(_bufs_listed()) do
+    buffers[i] = {
+      buf = b,
+      name = {
+        full = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(b), ':~:.'),
+        tail = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(b), ':t'),
+      },
+    }
+  end
+  _make_names_unique(buffers)
+end)
+
+local _state = {
+  loaded = false,
+  last_buf = nil,
+}
+
+local _buffers = {}
+
+local M = {}
+
+function M.setup()
+  if _state.loaded then return end
+  local augroup = vim.api.nvim_create_augroup('buffoo', { clear = true })
+  if vim.v.vim_did_enter == 1 then
+    _populate(_buffers)
+  else
+    vim.api.nvim_create_autocmd('VimEnter', {
+      group = augroup,
+      callback = function() _populate(_buffers) end,
+      once = true,
+    })
+  end
+  vim.api.nvim_create_autocmd('BufEnter', {
+    group = augroup,
+    callback = function(event)
+      local b = _buffers[_idx(event.buf, _buffers)]
+      if b then
+        _state.last_buf = b
+        if b.view then vim.fn.winrestview(b.view) end
+      end
+    end,
+  })
+  vim.api.nvim_create_autocmd('BufLeave', {
+    group = augroup,
+    callback = function(event)
+      for _, b in ipairs(_buffers) do
+        if b.buf == event.buf then
+          b.view = vim.fn.winsaveview()
+          break
+        end
+      end
+    end,
+  })
+  vim.api.nvim_create_autocmd('BufAdd', {
+    group = augroup,
+    callback = vim.schedule_wrap(function(event)
+      if vim.api.nvim_get_option_value('buftype', { buf = event.buf }) == '' then
+        local i = _idx(_state.last_buf, _buffers) or #_buffers
+        i = math.min(i + 1, #_buffers + 1)
+        table.insert(_buffers, i, {
+          buf = event.buf,
+          name = {
+            full = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(event.buf), ':~:.'),
+            tail = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(event.buf), ':t'),
+          },
+        })
+        _make_names_unique(_buffers)
+      end
+    end),
+  })
+  vim.api.nvim_create_autocmd('BufDelete', {
+    group = augroup,
+    callback = function(event)
+      if vim.api.nvim_get_option_value('buftype', { buf = event.buf }) == '' then
+        table.remove(_buffers, _idx(event.buf, _buffers))
+        _make_names_unique(_buffers)
+      end
+    end,
+  })
+  _state.last_buf = vim.api.nvim_get_current_buf()
+  _state.loaded = true
+end
+
+function M.buffers()
+  return _buffers
+end
+
+function M.switch(buf, direction, opts)
+  if #_buffers < 2 then return end
+  local cycle = opts and opts.cycle
+  direction = direction < 0 and -1 or 1
+  local i = _idx(buf, _buffers) + direction
+  if cycle then
+    if i < 1 then
+      i = #_buffers - i
+    elseif i > #_buffers then
+      i = i - #_buffers
+    end
+  else
+    if i < 1 or i > #_buffers then return end
+  end
+  vim.cmd.buffer(_buffers[i].buf)
+end
+
+function M.move(buf, direction, opts)
+  if #_buffers < 2 then return end
+  local cycle = opts and opts.cycle
+  direction = direction < 0 and -1 or 1
+  local i = _idx(buf, _buffers)
+  local j = i + direction
+  if cycle then
+    if j < 1 then
+      j = #_buffers - j
+    elseif j > #_buffers then
+      j = j - #_buffers
+    end
+  else
+    if j < 1 or j > #_buffers then return end
+  end
+  _buffers[i], _buffers[j] = _buffers[j], _buffers[i]
+end
+
+function M.nearest(buf)
+  local i = _idx(buf, _buffers)
+  local direction = i == 1 and 1 or -1
+  local j = i + direction
+  if j < 1 or j > #_buffers then return end
+  vim.cmd.buffer(_buffers[j].buf)
+end
+
+function M.close(args)
+  local force = args and args.force
+  local buf = vim.api.nvim_get_current_buf()
+  if not force and vim.api.nvim_get_option_value('modified', { buf = buf }) then
+    vim.notify('Won\'t close a modified buffer unless forced', vim.log.levels.WARN)
+    return
+  end
+  if #_bufs_listed() <= 1 then
+    vim.cmd.quit({ bang = force })
+  else
+    if
+      vim.fn.buflisted(buf) == 0 or
+      vim.fn.win_gettype(vim.api.nvim_get_current_win()) ~= '' or
+      vim.iter(ipairs(vim.api.nvim_list_wins())):filter(function(_, w) return vim.api.nvim_get_option_value('buftype', { buf = vim.api.nvim_win_get_buf(w) }) == '' end):nth(2)
+    then
+      vim.cmd.quit({ bang = force })
+    else
+      M.nearest(buf)
+    end
+    if #vim.fn.win_findbuf(buf) < 1 then
+      vim.cmd.bdelete({ buf, bang = force })
+    end
+  end
+end
+
+return M
